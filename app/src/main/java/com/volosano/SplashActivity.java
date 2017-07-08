5f75cc8185c7996ee7c8f63e0611e1dc65b90322
匿名者475 <1059876295@qq.com>
2017-07-08 23:59:40 +0800

    处理细节

--- app/src/main/java/com/volosano/SplashActivity.java
+++ app/src/main/java/com/volosano/SplashActivity.java
@@ -32,7 +32,7 @@ public class SplashActivity extends Activity {
             @Override
             public void run() {
                 timer.cancel();
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                startActivity(new Intent(SplashActivity.this, Picker.class));
                 handler.sendEmptyMessageDelayed(1, 300);
             }
         }, 2000);
